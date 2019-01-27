2c546bdfea197521644392260cbfbd90552915e6
Andrii Pereverziev <andriipereverziev@Andriis-MacBook-Pro.local>
2019-01-27 12:30:02 +0200

    Complete user model

--- server/server.js
+++ server/server.js
@@ -16,9 +16,25 @@ app.use(cookieParser());
 // Models
 const { User } = require('./models/user');
 
+// Middlewares
+const { auth } = require('./middleware/auth');
+
 //======================
 //        USERS
 //======================
+
+app.get('/api/users/auth', auth, (req, res) => {
+    res.status(200).json({
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        cart: req.user.cart,
+        history: req.user.history
+    });
+});
 
 app.post('/api/users/register', (req, res) => {
    const user = new User(req.body);
@@ -31,6 +47,39 @@ app.post('/api/users/register', (req, res) => {
           userdata: doc
       })
    });
+});
+
+app.post('/api/users/login', (req, res) => {
+
+    User.findOne({'email': req.body.email}, (err, user) => {
+        if(!user) return res.json({ loginSuccess: false, message: 'Auth failes, email not found!' });
+
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if(!isMatch) return res.json({ loginSuccess: false, message: 'Wrong password!' });
+
+            user.generateToken((err, user) => {
+                if(err) return res.status(400).send(err);
+                res.cookie('w_auth', user.token).status(200).json({
+                    loginSuccess: true
+                })
+            });
+        });
+    })
+});
+
+app.get('/api/users/logout', auth, (req, res)=>{
+
+    User.findOneAndUpdate(
+        { _id: req.user._id },
+        { token: '' },
+        (err, doc)=>{
+            if(err) return res.json({ success: false, err });
+            return res.status(200).send({
+                success: true
+            });
+        }
+    )
+
 });
 
 
